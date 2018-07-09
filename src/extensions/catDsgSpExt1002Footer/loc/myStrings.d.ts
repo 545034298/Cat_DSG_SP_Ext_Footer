675390433433d5780b9b1f715b8e6f1dcdb8fd8d
Roger Zhang <zhangqinyuan2013@live.cn>
2018-07-09 10:26:05 +0800

    Initialize Footer Extension

---
+++ src/extensions/catDsgSpExt1002Footer/loc/myStrings.d.ts
@@ -0,0 +1,8 @@
+declare interface ICatDsgSpExt1002FooterApplicationCustomizerStrings {
+  Title: string;
+}
+
+declare module 'CatDsgSpExt1002FooterApplicationCustomizerStrings' {
+  const strings: ICatDsgSpExt1002FooterApplicationCustomizerStrings;
+  export = strings;
+}
